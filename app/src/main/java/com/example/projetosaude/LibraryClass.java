2a51638e7d4bb9ff427a19c2b22eb8202c1e1efb
Emerson Borges <emerson2borges@users.noreply.github.com>
2018-12-16 20:48:42 -0300

    Login
    Acrescentando login

---
+++ app/src/main/java/com/example/projetosaude/LibraryClass.java
@@ -0,0 +1,15 @@
+package com.example.projetosaude;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public final class LibraryClass {
+    private static DatabaseReference firebase;
+    private LibraryClass(){}
+    public static DatabaseReference getFirebase(){
+        if( firebase == null ){
+            firebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return( firebase );
+    }
+}
